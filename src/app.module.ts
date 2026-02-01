6a834e306aa54444301c6cd09c5a93f50c27bb46
bikashroy120 <bikashroydt@gmail.com>
2026-02-01 13:36:19 +0600

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { CourseModule } from './course/course.module';
 
 @Module({
   imports: [
-    AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_DB as string),
-    UserModule,
-    CourseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
